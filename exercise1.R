ebc79050a4a05ea2e0974cb6c44e934566a0ef1c
Peyton Lyons <lyonsp@uw.edu>
2016-02-09 23:09:28 -0800

    Testing html

--- exercise1.R
+++ exercise1.R
@@ -3,19 +3,21 @@ library(jsonlite)
 
 # Create a variable `api_key` that stores your api key
 # Register your own API key here: https://developer.echonest.com/account/register
-
+api_key <- "IHZX7A5ZFJIFM0Q30"
 
 # Create a variable that stores the name of your favorite artist
-
+fav_artist <- "Magic Man"
 
 # Substitute the spaces in your band-name with '+'s using the `gsub` function
-
+band_name <- gsub(" ", "+", fav_artist)
 
 # Construct a seach query to hit the api.
 # See documentation/examples: http://developer.echonest.com/docs/v4/artist.html#biographies
-
+base_url <- "http://developer.echonest.com/api/v4/artist/biographies?api_key"
+query_url <- paste0(base_url, api_key, "&name=", band_name)
 
 # Use fromJSON to retrieve the results
-
+fav_results <- fromJSON(query_url)
 
 # Get the text of the (first) biography available
+first_bio <- fav_results$response$biographies$text[1]
--- exercise2.md
+++ exercise2.md
@@ -9,3 +9,10 @@ Edit this Markdown document below to display:
 - An image of the band (which you should download from the web, and display)
 
 ----
+My favorite band is Magic Man. The lead singer is Alex Caplow.
+Their best songs are:
+- Paris
+- Texas
+- Honey
+Here's a photo of them:
+![tbt](https://underneaththis.files.wordpress.com/2014/07/magicman_gtp_img_8238_photo-credit-gavin-thomas.jpg)
